f12cbba33a679953d9c232bc57738b05f514dfa3
Kenji Greene <kenjigrr@gmail.com>
2022-06-21 16:34:36 -0500

    added links to nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/navBar-component';
 
 function App() {
   return (
     <div className="App">
+      <header>
+        <NavBar />
+      </header>
     </div>
   );
 }
---
+++ src/components/navBar-component.js
@@ -0,0 +1,29 @@
+import React from "react";
+import './navBar-styles.css'
+
+
+function NavBar() {
+    return (
+        <div className="nav-cont">
+            <div className="navi left">
+                <nav>
+                    <a href="home">Home</a>
+                    <a href="products">Products</a>
+                    <a href="services">Sercives</a>
+                </nav>
+            </div>
+            <div className="middle">
+                <img src="public/photos/logo.jpg" alt='Commercial Creamery Logo' />
+            </div>
+            <div className="navi right">
+                <nav>
+                    <a href="news">News</a>
+                    <a href="careers">Careers</a>
+                    <a href="contacts">Contacts</a>
+                </nav>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
